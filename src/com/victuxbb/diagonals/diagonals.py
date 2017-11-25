807a2c797eef45b4edcb600c2f92f7cb70ee4b34
Victor <victuxbb@gmail.com>
2017-11-25 20:48:01 +0100

    Impossible permutations * Add filter to ignore all permutations that can't be achieved with the desired diagonals due of a lack of enought cells * Refactor from imperative way to a more declarative way * Add more tests

--- src/com/victuxbb/diagonals/diagonals.py
+++ src/com/victuxbb/diagonals/diagonals.py
@@ -1,4 +1,8 @@
 class Diagonals:
+    DIAGONAL_FROM_BOTTOM_LEFT_TO_TOP_RIGHT = 1
+    DIAGONAL_FROM_BOTTOM_RIGHT_TO_TOP_LEFT = 2
+    EMPTY = 0
+
     def __init__(self, size, diagonals):
         self.size = size
         self.diagonals = diagonals
@@ -9,47 +13,96 @@ class Diagonals:
 
     def can_be_extended_to_solution(self, perm, last_inserted):
         current_diagonal_value = perm[last_inserted]
-        current_row = last_inserted / self.size
 
-        if current_diagonal_value == 1:  # / BL-TR
-            left_cell = last_inserted - self.size
-            bottom_left_cell = last_inserted - self.size - 1
-            bottom_cell = last_inserted - 1
-            if perm[left_cell] == 2:
+        if current_diagonal_value == self.DIAGONAL_FROM_BOTTOM_LEFT_TO_TOP_RIGHT:
+            if self._is_left_cell_containing(
+                    perm,
+                    current_cell=last_inserted,
+                    expected=self.DIAGONAL_FROM_BOTTOM_RIGHT_TO_TOP_LEFT
+            ):
                 return False
-            if current_row - 1 == bottom_left_cell / self.size and perm[bottom_left_cell] == 1:
+            if self.is_bottom_left_cell_containing(
+                    perm,
+                    current_cell=last_inserted,
+                    expected=self.DIAGONAL_FROM_BOTTOM_LEFT_TO_TOP_RIGHT
+            ):
                 return False
-            if bottom_cell / self.size >= 0 and perm[bottom_cell] == 2:
+            if self._is_bottom_cell_containing(
+                    perm,
+                    current_cell=last_inserted,
+                    expected=self.DIAGONAL_FROM_BOTTOM_RIGHT_TO_TOP_LEFT
+            ):
                 return False
-        if current_diagonal_value == 2:  # \ BR-TL
-            top_left_cell = last_inserted - self.size + 1
-            left_cell = last_inserted - self.size
-            bottom_cell = last_inserted - 1
-            if current_row != top_left_cell / self.size and perm[top_left_cell] == 2:
+        if current_diagonal_value == self.DIAGONAL_FROM_BOTTOM_RIGHT_TO_TOP_LEFT:
+            if self.is_top_left_cell_containing(
+                    perm,
+                    current_cell=last_inserted,
+                    expected=self.DIAGONAL_FROM_BOTTOM_RIGHT_TO_TOP_LEFT
+            ):
                 return False
-            if perm[left_cell] == 1:
+            if self._is_left_cell_containing(
+                    perm,
+                    current_cell=last_inserted,
+                    expected=self.DIAGONAL_FROM_BOTTOM_LEFT_TO_TOP_RIGHT
+            ):
                 return False
-            if bottom_cell / self.size >= 0 and perm[bottom_cell] == 1:
+            if self._is_bottom_cell_containing(
+                    perm,
+                    current_cell=last_inserted,
+                    expected=self.DIAGONAL_FROM_BOTTOM_LEFT_TO_TOP_RIGHT
+            ):
                 return False
 
-        return True
+        return self._is_still_possible_to_achieve_desired_diagonals(perm, last_inserted)
 
     def extend(self, perm, start):
-        if -1 not in perm:
-            total = perm.count(1) + perm.count(2)
-            if total == self.diagonals:
-                print(perm)
+        total = perm.count(1) + perm.count(2)
+        if total == self.diagonals:
+            print(perm)
+            exit()
         if start < len(perm):
-            perm[start] = 1
+            perm[start] = self.DIAGONAL_FROM_BOTTOM_LEFT_TO_TOP_RIGHT
             if self.can_be_extended_to_solution(perm, last_inserted=start):
                 next_start = start + 1
                 self.extend(perm, start=next_start)
 
-            perm[start] = 2
+            perm[start] = self.DIAGONAL_FROM_BOTTOM_RIGHT_TO_TOP_LEFT
             if self.can_be_extended_to_solution(perm, last_inserted=start):
                 next_start = start + 1
                 self.extend(perm, start=next_start)
 
-            perm[start] = 0
+            perm[start] = self.EMPTY
             next_start = start + 1
             self.extend(perm, start=next_start)
+
+    def _is_still_possible_to_achieve_desired_diagonals(self, perm, last_inserted):
+        current_number_of_diagonals = perm.count(1) + perm.count(2)
+        if len(perm) - 1 - last_inserted < self.diagonals - current_number_of_diagonals:
+            return False
+        return True
+
+    def _is_left_cell_containing(self, perm, current_cell, expected):
+        left_cell = current_cell - self.size
+        if perm[left_cell] == expected:
+            return True
+        return False
+
+    def _is_bottom_cell_containing(self, perm, current_cell, expected):
+        bottom_cell = current_cell - 1
+        if bottom_cell / self.size >= 0 and perm[bottom_cell] == expected:
+            return True
+        return False
+
+    def is_top_left_cell_containing(self, perm, current_cell, expected):
+        top_left_cell = current_cell - self.size + 1
+        current_row = current_cell / self.size
+        if current_row != top_left_cell / self.size and perm[top_left_cell] == expected:
+            return True
+        return False
+
+    def is_bottom_left_cell_containing(self, perm, current_cell, expected):
+        bottom_left_cell = current_cell - self.size - 1
+        current_row = current_cell / self.size
+        if current_row - 1 == bottom_left_cell / self.size and perm[bottom_left_cell] == expected:
+            return True
+        return False
--- src/tests/test_diagonals.py
+++ src/tests/test_diagonals.py
@@ -31,3 +31,11 @@ class TestDiagonals(TestCase):
     def test_given_BR_TL_with_top_left_cell_diagonal_error_it_should_return_false(self):
         given_permutation = [0, 2, 2, 1, 2, -1, -1, -1, -1]
         self.assertFalse(self._diagonals.can_be_extended_to_solution(perm=given_permutation, last_inserted=4))
+
+    def test_given_number_of_cells_not_processed_it_should_not_try_to_extend(self):
+        given_permutation = [1, 0, 0, 0, 0, -1, -1, -1, -1]
+        self.assertFalse(self._diagonals.can_be_extended_to_solution(perm=given_permutation, last_inserted=4))
+
+    def test_given_number_of_cells_not_processed_it_should_try_to_extend(self):
+        given_permutation = [1, 1, 1, 0, 0, 0, 1, -1, -1]
+        self.assertTrue(self._diagonals.can_be_extended_to_solution(perm=given_permutation, last_inserted=6))
